31bedcff1be980debd4cb7b78a33ff1ec32fe6db
Oizys13 <brchreda@gmail.com>
2024-08-01 16:26:31 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import os
+
+movies = pd.read_csv("Data/tmdb_5000_movies")
+credits = pd.read_csv("Data/tmdb_5000_credits")
